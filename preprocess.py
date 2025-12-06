f4e5f606a578f926f35651806ecb597b57cc562a
awcheng23 <awcheng23@gmail.com>
2025-12-06 14:35:03 -0500

    Add stride data retrieval

--- preprocess.py
+++ preprocess.py
@@ -1,39 +1,50 @@
 import numpy as np
-from data_dir.process_mitdb import get_patients_segments, split_train_test, get_sampled_data
+from data_dir.process_mitdb import (
+    get_patients_segments,
+    split_train_val_test,
+    get_sampled_data,
+)
 from data_dir import PATIENT_IDS
 
+
 def main():
-    beats = []
-    beat_IDs = []
-    data_path = 'data/mitdb/'
+    segments = []
+    segment_IDs = []
+    data_path = "data/mitdb/"
 
-    for id in PATIENT_IDS: # take out patients for testing
-        pat_beats, pat_beat_ID = get_patients_segments(ID=id, dt_path=data_path)
+    for id in PATIENT_IDS:  # take out patients for testing
+        pat_segments, pat_segment_ID = get_patients_segments(
+            ID=id, dt_path=data_path, window_sec=50.0, stride_sec=2.5, target_fs=100.0
+        )
 
-        beats.extend(pat_beats)
-        beat_IDs.extend(pat_beat_ID)
+        segments.extend(pat_segments)
+        segment_IDs.extend(pat_segment_ID)
 
     print("Completed data load")
 
-    train_dist, test_dist = split_train_test(beat_IDs)
-    beats_train, beat_IDs_train = get_sampled_data(beats, beat_IDs, train_dist, num_samples=2500, augment=True)
-    beats_test, beat_IDs_test = get_sampled_data(beats, beat_IDs, test_dist, num_samples=800)
+    train_dist, val_dist, test_dist = split_train_val_test(segment_IDs)
+    segments_train, segment_IDs_train = get_sampled_data(
+        segments, segment_IDs, train_dist, num_samples=15577, augment=True
+    )
+    segments_val, segment_IDs_val = get_sampled_data(
+        segments, segment_IDs, val_dist, num_samples=1302
+    )
+    segments_test, segment_IDs_test = get_sampled_data(
+        segments, segment_IDs, test_dist, num_samples=1302
+    )
     print("Completed data sampling")
 
-    # scalograms_train = cwt_parallel(beats=beats_train, widths=widths)
-    # scalograms_test = cwt_parallel(beats=beats_test, widths=widths)
-    # print("Completed cwt")
+    np.savez_compressed(
+        "data/db_train_anomaly.npz", segments=segments_train, labels=segment_IDs_train
+    )
+    np.savez_compressed(
+        "data/db_val_anomaly.npz", segments=segments_val, labels=segment_IDs_val
+    )
+    np.savez_compressed(
+        "data/db_test_anomaly.npz", segments=segments_test, labels=segment_IDs_test
+    )
+    print("Completed file save")
 
-    # max_length = max(max([scalogram.shape[1] for scalogram in scalograms_train]), max([scalogram.shape[1] for scalogram in scalograms_test]))
-    # scalograms_train = pad_scalograms(scalograms_train, max_length=max_length)
-    # scalograms_test = pad_scalograms(scalograms_test, max_length=max_length)
-    # print("Completed padding")
 
-    # np.savez_compressed("data/db_31_train_2500_aami.npz", scalograms=scalograms_train, labels=beat_IDs_train)
-    # np.savez_compressed("data/db_31_test_2500_aami.npz", scalograms=scalograms_test, labels=beat_IDs_test)
-    
-    # print("Completed file save")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
